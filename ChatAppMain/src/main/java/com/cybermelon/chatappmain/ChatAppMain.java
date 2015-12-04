1e9d5a2d6dfbd416797f9f7ad95e285221c51683
daimon_sewell <daimon.sewell@rbgapps.com>
2015-12-04 12:15:49 +0200

    ChatAppMain Gradle based initial Commit

---
+++ ChatAppMain/src/main/java/com/cybermelon/chatappmain/ChatAppMain.java
@@ -0,0 +1,15 @@
+package com.cybermelon.chatappmain;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class ChatAppMain extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chat_app_main);
+    }
+}
